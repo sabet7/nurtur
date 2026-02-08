b1e24927ca3206638e0c6d3a5599b0b35106d914
Sara <69481470+sabet7@users.noreply.github.com>
2026-02-08 11:10:23 -0500

    Refactor WatercolorDecoration components in landing view

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-
+// JJ
 import React, { useState, useEffect } from 'react';
 import { UserRole, UserProfile } from './types';
 import Onboarding from './components/Onboarding';
@@ -146,14 +146,24 @@ const App: React.FC = () => {
   if (view === 'landing') {
     return (
       <div className="min-h-screen flex flex-col items-center justify-center bg-[#fcfdfa] p-6 relative overflow-hidden font-sans">
-        <WatercolorDecoration type="bell pepper" className="top-[5%] left-[10%] w-64 h-64 rotate-[15deg]" />
-        <WatercolorDecoration type="cherry" className="top-[15%] right-[5%] w-56 h-56 rotate-[-10deg]" />
-        <WatercolorDecoration type="cucumber" className="bottom-[10%] left-[5%] w-72 h-72 rotate-[-20deg]" />
-        <WatercolorDecoration type="carrots" className="bottom-[5%] right-[10%] w-64 h-64 rotate-[25deg]" />
-        <WatercolorDecoration type="pomegranate" className="top-[40%] -left-20 w-80 h-80 opacity-20" />
-        <WatercolorDecoration type="onion" className="top-[50%] -right-20 w-80 h-80 opacity-20" />
-        <WatercolorDecoration type="fennel flower" className="bottom-0 left-1/2 -translate-x-1/2 w-[800px] h-96 opacity-10" />
-        <WatercolorDecoration type="garlic" className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] opacity-10" />
+        <div className="absolute top-[5%] left-[5%] w-56 h-56 opacity-40 pointer-events-none z-0" style={{ transform: 'rotate(15deg)' }}>
+          <WatercolorDecoration type="bell pepper" className="w-full h-full" />
+        </div>
+        <div className="absolute top-[8%] right-[8%] w-48 h-48 opacity-40 pointer-events-none z-0" style={{ transform: 'rotate(-12deg)' }}>
+          <WatercolorDecoration type="cherry" className="w-full h-full" />
+        </div>
+        <div className="absolute bottom-[25%] left-[2%] w-64 h-64 opacity-35 pointer-events-none z-0" style={{ transform: 'rotate(-20deg)' }}>
+          <WatercolorDecoration type="cucumber" className="w-full h-full" />
+        </div>
+        <div className="absolute bottom-[5%] right-[3%] w-60 h-60 opacity-40 pointer-events-none z-0" style={{ transform: 'rotate(18deg)' }}>
+          <WatercolorDecoration type="carrots" className="w-full h-full" />
+        </div>
+        <div className="absolute top-[50%] -left-32 w-64 h-64 opacity-15 pointer-events-none z-0" style={{ transform: 'rotate(25deg)' }}>
+          <WatercolorDecoration type="pomegranate" className="w-full h-full" />
+        </div>
+        <div className="absolute top-[40%] -right-28 w-56 h-56 opacity-15 pointer-events-none z-0" style={{ transform: 'rotate(-18deg)' }}>
+          <WatercolorDecoration type="garlic" className="w-full h-full" />
+        </div>
 
         <div className="z-10 text-center max-w-2xl px-4 animate-in fade-in slide-in-from-bottom-8 duration-1000">
           <div className="mb-12">
@@ -216,3 +226,4 @@ const App: React.FC = () => {
 };
 
 export default App;
+// SDG
